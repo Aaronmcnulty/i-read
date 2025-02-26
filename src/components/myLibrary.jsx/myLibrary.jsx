ac105dd86cd21ddbfc957071d974643002118441
malcomcrab <malcomcrabesq@outlook.com>
2025-02-26 21:03:05 +0000

    Feature: My Library component drop down menu chooses which book list is rendered (read, owned, wishlist)

--- src/components/myLibrary.jsx/myLibrary.jsx
+++ src/components/myLibrary.jsx/myLibrary.jsx
@@ -5,16 +5,31 @@ import BooksDisplay from "../bookDisplay/BookDisplay";
 function MyLibrary(){
     
     const context = useContext(LibraryContext)
-
     
-    console.log(context.ownedBooksArray)
-    console.log(context.readBooksArray)
-    console.log(context.wishListBooksArray)
+    const lists = [
+        context.ownedBooksArray,
+        context.readBooksArray,
+        context.wishListBooksArray
+    ]
+    
+    const [option, setOption] = useState(0)
+
+    const  changeOption = (event) => {
+        setOption(event.target.value)
+    }
+
     return(
         <>
             <h2>My Library</h2>
             <div>
-             <BooksDisplay BookApiData={context.ownedBooksArray} />
+                <select onChange={changeOption}>
+                    <option value="0">Owned Books</option>
+                    <option value="1">Read Books</option>
+                    <option  value="2">Wishlist</option>
+                </select>
+            </div>
+            <div>
+             <BooksDisplay BookApiData={lists[option]} />
             </div>          
         </>
     )
